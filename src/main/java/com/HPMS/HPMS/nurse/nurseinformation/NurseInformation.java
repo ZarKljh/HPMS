35fdbf6958fe0f633b483269c1456318ac6cdd40
jiseon723 <dighxxx12@gmail.com>
2025-08-26 18:42:48 +0900

    branch

---
+++ src/main/java/com/HPMS/HPMS/nurse/nurseinformation/NurseInformation.java
@@ -0,0 +1,12 @@
+package com.HPMS.HPMS.nurse.nurseinformation;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class NurseInformation {
+
+}
