9baf4755a59b2fb75e69e8dab05f27c103fe8a8e
jiseon723 <dighxxx12@gmail.com>
2025-08-27 09:44:02 +0900

    entity

--- src/main/java/com/HPMS/HPMS/nurse/license/License.java
+++ src/main/java/com/HPMS/HPMS/nurse/license/License.java
@@ -1,11 +1,34 @@
 package com.HPMS.HPMS.nurse.license;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@Table(name = "LICENSE")
 public class License {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Integer id;
+
+    @Column(nullable = false, name = "NURSE_ID")
+    private Integer nurseId;
+
+    @Column(length = 250, name = "LI")
+    private String li;
+
+    @Column(length = 20, name = "LICENSE_NO")
+    private String licenseNo;
+
+    @Column(length = 8, name = "ISSUE_DATE")
+    private String issueDate;
+
+    @Column(length = 8, name = "EXPIRY_DATE")
+    private String expiryDate;
+
+    @Column(length = 250, name = "NOTE")
+    private String note;
 }
