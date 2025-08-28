9f333229d1b33e83be7819fd08700713f2482270
whenyouwerelittle <whenyouwerelittle@gmail.com>
2025-08-28 17:49:07 +0900

    first commit

--- src/main/java/com/HPMS/HPMS/reference_personnel/reference_personnel_dtl/ReferencePersonnelDtlRepository.java
+++ src/main/java/com/HPMS/HPMS/reference_personnel/reference_personnel_dtl/ReferencePersonnelDtlRepository.java
@@ -3,5 +3,5 @@ package com.HPMS.HPMS.reference_personnel.reference_personnel_dtl;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReferencePersonnelDtlRepository extends JpaRepository<ReferencePersonnelDtl, Integer> {
-
+    ReferencePersonnelDtl findByFirstName(String firstName);
 }
