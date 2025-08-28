2e90d79a0fd945d881215f1f9894c2ec4e68bf3d
jiseon723 <dighxxx12@gmail.com>
2025-08-28 18:45:45 +0900

    nurse HTML_1

--- src/main/java/com/HPMS/HPMS/nurse/nursemain/NurseMainController.java
+++ src/main/java/com/HPMS/HPMS/nurse/nursemain/NurseMainController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -13,10 +14,17 @@ public class NurseMainController {
     @Autowired
     private NurseMainService nurseMainService;
 
-    @GetMapping("/nurse/list")
+    @GetMapping("/nurse")
     private String list(Model model) {
         List<NurseMain> nurseMainList = this.nurseMainService.getAll();
         model.addAttribute("nurseMainList", nurseMainList);
         return "nurse/nurse_main";
+    }
+
+    @GetMapping(value = "/nurse/information/{id}")
+    public String information(Model model, @PathVariable("id") Integer id) {
+        NurseMain nurseMain = this.nurseMainService.getNurseMain(id);
+        model.addAttribute("nurseMain", nurseMain);
+        return "nurse/nurse_information";
     }
 }
--- src/main/java/com/HPMS/HPMS/nurse/nursemain/NurseMainService.java
+++ src/main/java/com/HPMS/HPMS/nurse/nursemain/NurseMainService.java
@@ -1,9 +1,11 @@
 package com.HPMS.HPMS.nurse.nursemain;
 
+import com.HPMS.HPMS.nurse.NurseDataNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class NurseMainService {
@@ -13,5 +15,14 @@ public class NurseMainService {
 
     public List<NurseMain> getAll() {
         return nurseMainRepository.findAll();
+    }
+
+    public NurseMain getNurseMain(Integer id) {
+        Optional<NurseMain> nurseMain = this.nurseMainRepository.findById(id);
+        if (nurseMain.isPresent()) {
+            return nurseMain.get();
+        } else {
+            throw new NurseDataNotFoundException("nurse not found");
+        }
     }
 }
