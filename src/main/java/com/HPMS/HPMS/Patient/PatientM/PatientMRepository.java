5abdd06e194509142e6f6936bc675937c7cc53b3
ZarKljh <zarkljh@gmail.com>
2025-09-09 00:25:47 +0900

    multi condition search add

--- src/main/java/com/HPMS/HPMS/Patient/PatientM/PatientMRepository.java
+++ src/main/java/com/HPMS/HPMS/Patient/PatientM/PatientMRepository.java
@@ -5,12 +5,14 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PatientMRepository extends JpaRepository<PatientM, Integer> {
     Page<PatientM> findAll(Pageable pageable);
+    Page<PatientM> findAll(Specification<PatientM> spec, Pageable pageable);
 }
 
 
