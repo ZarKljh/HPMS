e5689c1bf43deaed402e6676e6952698b7be9c4a
ZarKljh <zarkljh@gmail.com>
2025-09-09 18:50:12 +0900

    address iuput adding

--- src/main/java/com/HPMS/HPMS/global/Address_Code/Address_Code_Service.java
+++ src/main/java/com/HPMS/HPMS/global/Address_Code/Address_Code_Service.java
@@ -1,4 +1,16 @@
+// src/main/java/com/HPMS/HPMS/global/Address_Code/Address_Code_Service.java
 package com.HPMS.HPMS.global.Address_Code;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.*;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class Address_Code_Service {
+    private final Address_Code_Repository repository;
+
+    public Page<Address_Code> search(String kw, Pageable pageable) {
+        return repository.search(kw, pageable);
+    }
 }
