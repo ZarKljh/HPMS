c5f4b018cbbb5c8a8161c68fa474a41ab6edd95f
ZarKljh <zarkljh@gmail.com>
2025-09-10 18:46:02 +0900

    login&logout update

--- src/main/java/com/HPMS/HPMS/SecurityConfig.java
+++ src/main/java/com/HPMS/HPMS/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.HPMS.HPMS;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -17,24 +18,49 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+//                .authorizeHttpRequests((auth) -> auth
+//                        .requestMatchers("/css/**", "/js/**", "/images/**").permitAll())
+
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/", "/hpms/login", "/hpms/signup", "/error").permitAll()
+                        .requestMatchers("/CSS/**", "/js/**", "/img/**").permitAll()
+                        .anyRequest().authenticated())
+                /*
                 .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
                         .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/**").permitAll())
+                */
                 .csrf((csrf) -> csrf
-                        .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")))
+                        .ignoringRequestMatchers("/h2-console/**"))
+//                .csrf((csrf) -> csrf
+//                        .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")))
                 .headers((headers) -> headers
                         .addHeaderWriter(new XFrameOptionsHeaderWriter(
                                 XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
+//                .formLogin((formLogin) -> formLogin
+//                        .loginPage("/hpms/login")
+//                        .defaultSuccessUrl("/"))
                 .formLogin((formLogin) -> formLogin
-                        .loginPage("/hpms/login")
-                        .defaultSuccessUrl("/"))
+                        .loginPage("/hpms/login")         // 로그인 폼 경로
+                        .loginProcessingUrl("/hpms/login") // 로그인 처리 경로 (POST)
+                        .defaultSuccessUrl("/", true)          // 로그인 성공 후 이동 경로
+                        .permitAll())
                 .logout((logout) -> logout
                         .logoutRequestMatcher(new AntPathRequestMatcher("/hpms/logout"))
                         .logoutSuccessUrl("/")
                         .invalidateHttpSession(true))
-//                사용자가 권한부족으로 페이지접속이 거부되는 경우
+
+
+//                .logout((logout) -> logout
+//                        .logoutRequestMatcher(new AntPathRequestMatcher("/hpms/logout"))
+//                        .logoutSuccessUrl("/hpms/login")
+//                        .invalidateHttpSession(true))
+////                사용자가 권한부족으로 페이지접속이 거부되는 경우
 //                .exceptionHandling(exception -> exception
 //                        .accessDeniedHandler(new CustomAccessDeniedHandler())
 //                );
