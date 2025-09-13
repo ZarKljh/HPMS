4ee6b8543632af484f5e715927b9408d02ce57aa
jiseon723 <dighxxx12@gmail.com>
2025-09-13 16:54:01 +0900

    html_7

--- src/main/java/com/HPMS/HPMS/SecurityConfig.java
+++ src/main/java/com/HPMS/HPMS/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.HPMS.HPMS;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
