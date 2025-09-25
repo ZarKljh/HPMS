3763360f80dd223b885a61c7e9dd1a5529771261
ZarKljh <zarkljh@gmail.com>
2025-09-26 00:28:38 +0900

    css modifying

--- src/main/resources/static/js/patient/patient.js
+++ src/main/resources/static/js/patient/patient.js
@@ -166,8 +166,166 @@ $('.navbar-toggle').click(function() {
         }
 
         form.submit();
+    }
+
+
+    /* create form validate 시작 */
+
+    function validateModifyForm(event){
+
+        event.preventDefault(); // 폼 제출 막기
+
+        const form = document.forms['patientModifyForm'];
+        /*
+        const birth = form.querySelector("input[name='birth']");
+        const startDate = new Date('1900-01-01');
+        const nowDate = new Date();
+        const formatBirth = new Date(birth.value);
+        if(!birth.value) {
+            alert("날짜를 입력해주세요.");
+            return false;
+        } else if(formatBirth < startDate) {
+            alert("생년월일은 1900년1월1일 이전은 입력할 수 없습니다");
+            return false;
+        } else if(formatBirth > nowDate) {
+            alert("생년월일은 오늘날짜 이후 날짜는 입력할 수 없습니다");
+            return false;
+        }
+        */
+        const gender = form.querySelector("select[name='gender']");
+        if(!gender.value) {
+            alert("성별을 선택해주세요.");
+            return false;
+        } else if(gender.value != 'M' && gender.value != 'F' && gender.value !='X'){
+            alert("올바른 값을 선택해주세요.");
+            return false;
         }
 
+        const foreigner = form.querySelector("select[name='foreigner']");
+        if(foreigner.value != 0 && foreigner.value != 1) {
+            alert("내외국인 여부를 선택해주세요");
+            return false;
+        }
+
+        const nameRegexHan = /^[가-힣]+$/;
+        const nameRegexEng = /^[a-zA-Z]+$/;
+        const nameRegexEngCapital = /^[A-Z]+$/;
+        const nameRegex = /^[가-힣a-zA-Z]+$/;
+
+        const firstNameInput = form.querySelector("input[name=firstName]");
+        const lastNameInput = form.querySelector("input[name=lastName]");
+        const middleNameInput = form.querySelector("input[name=middleName]");
+
+        /*이름 앞뒤 빈칸제거 + 중간빈칸제거*/
+
+        let firstName = firstNameInput.value.trim().replace(/ /g,"");
+        let lastName = lastNameInput.value.trim().replace(/ /g,"");
+        let middleName = middleNameInput.value.trim().replace(/ /g,"");
+
+        firstNameInput.value = firstName;
+        lastNameInput.value = lastName;
+        middleNameInput.value = middleName;
+
+        if(!nameRegexHan.test(firstName) || !nameRegexHan.test(lastName) || !nameRegexHan.test(middleName)){
+            alert("성명 성과 이름, 중간이름 한글로 작성해주세요");
+            return false;
+        } else if(firstName.length < 1 || lastName.length < 1) {
+            alert("성명 성과 이름은 필수항목입니다");
+            return false;
+        }
+
+        const passport = form.querySelector("input[name='passport']");
+        /*여권번호를 입력했을 때에만 검증*/
+        if(passport.value.length > 1) {
+            /*입력된 가장 첫자리만 오려서 가져온다*/
+            const passportPartEng = passport.value.slice(0,1);
+            /*입력된 여권번호의 가장 첫자리가 영문 대문자인지 검증*/
+            if(!nameRegexEngCapital.test(passportPartEng)){
+                alert("여권번호의 가장 앞자리는 영문 대문자입니다");
+                return false;
+            /*입력된 여권번호가 8자리 인지 검증*/
+            } else if (passport.value.length != 8) {
+                alert("여권번호는 8자리 입니다");
+                return false;
+            }
+        }
+
+        const passFirstNameInput = form.querySelector("input[name=passFirstName]");
+        const passLastNameInput = form.querySelector("input[name=passLastName]");
+        const passMiddleNameInput = form.querySelector("input[name=passMiddleName]");
+
+        /*이름 앞뒤 빈칸제거 + 중간빈칸제거*/
+
+        let passFirstName = passFirstNameInput.value.trim().replace(/ /g,"");
+        let passLastName = passLastNameInput.value.trim().replace(/ /g,"");
+        let passMiddleName = passMiddleNameInput.value.trim().replace(/ /g,"");
+
+        passFirstNameInput.value = passFirstName;
+        passLastNameInput.value = passLastName;
+        passMiddleNameInput.value = passMiddleName;
+
+        /*여권번호를 작성했을 시에만 검증*/
+        if(passport.value.length > 1){
+            if(!nameRegexEng.test(passFirstName) || !nameRegexEng.test(passLastName) || !nameRegexEng.test(passMiddleName)){
+                alert("여권 성명 성과 이름, 중간이름 영문으로 작성해주세요");
+                return false;
+            } else if(firstName.length < 1 || lastName.length < 1) {
+                alert("성명 성과 이름은 필수항목입니다");
+                return false;
+            }
+        }
+        /*숫자와 자릿수 설정*/
+        const numberOnlyRegex = /^[0-9]{9,12}$/;
+        /*휴대전화번호 검증*/
+        const mobilePhone = form.querySelector("input[name='mobilePhone']").value.trim();
+        if(mobilePhone.length === 0) {
+            alert("휴대전화번호는 필수항목입니다");
+            return false;
+        } else if(!numberOnlyRegex.test(mobilePhone)) {
+            alert("연락처에는 숫자만 넣어주세요");
+            return false;
+        }
+
+        /*보호자 정보 검증*/
+        const guardianFirstNameInput = form.querySelector("input[name=guardianFirstName]");
+        const guardianLastNameInput = form.querySelector("input[name=guardianLastName]");
+
+        let guardianFirstName = guardianFirstNameInput.value.trim().replace(/ /g,"");
+        let guardianLastName = guardianLastNameInput.value.trim().replace(/ /g,"");
+
+        guardianFirstNameInput.value = guardianFirstName;
+        guardianLastNameInput.value = guardianLastName;
+
+        if(guardianFirstName.length < 1 || guardianLastName.length < 1) {
+            alert("보호자 성명 성과 이름은 필수 항목입니다");
+            return false;
+        } else if(!nameRegex.test(guardianFirstName) || !nameRegex.test(guardianLastName)){
+            alert("보호자 성명 성과 이름은 한글 혹은 영문으로 작성해주세요");
+            return false;
+        }
+
+
+        /*휴대전화번호 검증*/
+        const guardianTel = form.querySelector("input[name='guardianTel']").value.trim();
+        if(guardianTel.length === 0) {
+            alert("보호자 연락처는 필수항목입니다");
+            return false;
+        } else if(!numberOnlyRegex.test(guardianTel)) {
+            alert("연락처에는 숫자만 넣어주세요");
+            return false;
+        }
+        const numberOnlyRegexForPcd = /^[0-9]{5,6}$/;
+        const homePcd = form.querySelector("input[name='homePcd']").value.trim();
+        if( homePcd.length  === 0 ){
+            alert("실거주지 우편번호는 필수항목입니다");
+            return false;
+        } else if (!numberOnlyRegexForPcd.test(homePcd)) {
+            alert("우편번호는 5자리 혹은 6자리의 숫자만 입력해주세요");
+            return false;
+        }
+
+        form.submit();
+    }
 
     /*체크박스 체크된 환자정보 삭제기능*/
     /* 제한된 체크박스 확인 후, 폼 COMMIT하기 */
